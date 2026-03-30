a50067f33a35b2ac3bd05327ef705d09ed2804ea
Ahmed0063 <geroevahmed4@gmail.com>
2026-03-30 19:27:28 +0300

    Update vkbot.py

--- vkbot.py
+++ vkbot.py
@@ -1,7 +1,7 @@
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 
-TOKEN = "ТВОЙ_ТОКЕН_ГРУППЫ"
+TOKEN = "vk1.a.fZRNT6_hXAvYyoIOh6weXII9fvs7ujQ-gyvWF0XfTOV4vo6Ax1Y8wzAqqKkp4k51Fk0K1SAGxfv9VngQoI8clKUlaL152m4Rbtlmc48B7btc2pb2iHD-aesWn4j3WaHN3Bakk7SSyLRzjPOx94JtnQAepBEOBk1-t4S5A_cpjNSSN49jZmGmHBI0cSrD1kwVbq2J7tHW5XK0yfNo6PPF9Q"
 GROUP_ID = 237041501  # твой ID группы
 
 vk_session = vk_api.VkApi(token=TOKEN)
